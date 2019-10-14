90696b3249c79439001eed8000f2e8baf4169dcd
yuki7827 <yuki7827@gmail.com>
2019-10-15 01:23:20 +0900

    プロジェクト作成

---
+++ EverydayApp/EverydayApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  EverydayApp
+//
+//  Created by Apple on 2019/10/15.
+//  Copyright © 2019年 Baminami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
